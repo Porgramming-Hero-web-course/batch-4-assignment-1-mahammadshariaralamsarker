d4f3103d821275fe68196db67ed6a6b4b05018f0
Shariar <mahammadshariaralam@gmail.com>
2024-11-10 16:42:30 +0600

    problem-1

--- problem/problem-1.js
+++ problem/problem-1.js
@@ -1,2 +1,8 @@
-console.log("o9");
-console.log("all is ok to complile");
+function sumArray(numbers) {
+    var sum = 0;
+    for (var i = 0; i <= numbers.length; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
+console.log(sumArray([1, 2, 3, 4, 5]));
--- problem/problem-1.ts
+++ problem/problem-1.ts
@@ -1,2 +1,9 @@
-console.log("o9");
-console.log("all is ok to complile");
+ function sumArray(numbers:number[]):number{
+    let sum = 0;
+      for(let i=0 ; i<=numbers.length; i++){
+        sum = sum+i;
+
+      }
+      return sum;
+ }
+ console.log(sumArray([1,2,3,4,5]));
