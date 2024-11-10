ef1ba39cf3b7b6602d8f78e2bed395689d40615b
Shariar <mahammadshariaralam@gmail.com>
2024-11-10 17:54:00 +0600

    problem-7

---
+++ problem/problem-7.js
@@ -0,0 +1,12 @@
+var Car = /** @class */ (function () {
+    function Car(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    Car.prototype.getCarAge = function () {
+        var currentYear = new Date().getFullYear();
+        return currentYear - this.year;
+    };
+    return Car;
+}());
